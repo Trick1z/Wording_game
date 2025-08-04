5ed89b7ef5cb5c40a7ee95c5051f53e6e258893f
trick1z <tiker.zone.teen@gmail.com>
2025-08-04 17:50:33 +0700

    เพิ่ม Route to Register page

--- src/app/Components/Pages/login/login.component.ts
+++ src/app/Components/Pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthRoute } from 'src/app/Constants/routes.const';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+ constructor(
+  private Route :Router
+ ){}
 
+
+
+ NavigateToRegisterPage(){
+
+  return this.Route.navigate([AuthRoute.RegisterFullPath])
+ }
 }
