aa374f1d06ed386dc594a785f156cf3bd19295d0
trick1z <tiker.zone.teen@gmail.com>
2025-08-04 16:59:34 +0700

    เพิ่ม View Route ,Game Route

--- src/app/Constants/routes.const.ts
+++ src/app/Constants/routes.const.ts
@@ -6,31 +6,17 @@ export class AuthRoute {
     // register path
     static Register: string = `register`;
     static RegisterFullPath: string = `${AuthRoute.prefix}/${AuthRoute.Register}`;
+    
+}
+export class ViewsRoute {
+    static prefix: string = `view`;
 
-    // static TransferRequestEntry: string = `transfer-request-entry`;
-    // static TransferRequestEntryFullPath: string = `${TransferRoute.prefix}/${TransferRoute.TransferRequestEntry}`;
+    static Home: string = `home`;
+    static HomeFullPath: string = `${ViewsRoute.prefix}/${ViewsRoute.Home}`;
+}
+export class GamesRoute {
+    static prefix: string = `game`;
 
-    // static TransferRequestView: string = `transfer-request-view`;
-    // static TransferRequestViewFullPath: string = `${TransferRoute.prefix}/${TransferRoute.TransferRequestView}`;
-
-    // static TransferApproveList: string = `transfer-approve-list`;
-    // static TransferApproveListFullPath: string = `${TransferRoute.prefix}/${TransferRoute.TransferApproveList}`;
-
-    // static TransferApproveEntry: string = `transfer-approve-entry`;
-    // static TransferApproveEntryFullPath: string = `${TransferRoute.prefix}/${TransferRoute.TransferApproveEntry}`;
-
-    // static TransferRequestAccountList: string = `transfer-request-account-list`
-    // static TransferRequestAccountListFullPath: string = `${TransferRoute.prefix}/${TransferRoute.TransferRequestAccountList}`
-
-    // static TransferRequestAccountEntry: string = `transfer-request-account-entry`
-    // static TransferRequestAccountEntryFullPath: string = `${TransferRoute.prefix}/${TransferRoute.TransferRequestAccountEntry}`
-
-    // static TransferRequestFOList: string = `transfer-request-fo-list`
-    // static TransferRequestFOListFullPath: string = `${TransferRoute.prefix}/${TransferRoute.TransferRequestFOList}`
-
-    // static TransferRequestFOEntry: string = `transfer-request-fo-entry`
-    // static TransferRequestFOEntryFullPath: string = `${TransferRoute.prefix}/${TransferRoute.TransferRequestFOEntry}`
-
-    // static TransferRequestAdmin: string = `transfer-request-admin`
-    // static TransferRequestAdminFullPath: string = `${TransferRoute.prefix}/${TransferRoute.TransferRequestAdmin}`
+    static Word: string = `word-scoring`;
+    static WordFullPath: string = `${GamesRoute.prefix}/${GamesRoute.Word}`;
 }
