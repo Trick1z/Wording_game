4543bef45dff1639d238ee594ed5d4539461d84a
trick1z <tiker.zone.teen@gmail.com>
2025-08-05 12:03:34 +0700

    ดัก Navbar ไม่ให้แสดงที่หน้า Login  ,register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+import { AuthRoute } from './Constants/routes.const';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'ClientApp';
+
+  showNavbar = false;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
+      .subscribe(event => {
+        const hiddenRoutes = ['/', '/auth/login', '/auth/register'];
+        this.showNavbar = !hiddenRoutes.includes(event.urlAfterRedirects);
+        console.log('Current URL:', event.urlAfterRedirects, 'showNavbar:', this.showNavbar);
+      });
+  }
+
 }
