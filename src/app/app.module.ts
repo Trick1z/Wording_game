3d608fcfd55a6725a49e2213855e661bdd6e209b
trick1z <tiker.zone.teen@gmail.com>
2025-08-05 12:05:22 +0700

    import ตัวที่ต้องใช้งาน

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule,CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +7,10 @@ import { LoginComponent } from './Components/Pages/login/login.component';
 import { RegisterComponent } from './Components/Pages/register/register.component';
 import { HomeComponent } from './Components/views/home/home.component';
 import { WordScoringComponent } from './Components/games/word-scoring/word-scoring.component';
+import { FormsModule } from '@angular/forms';
+import dxDataGrid from 'devextreme/ui/data_grid';
+import { DxButtonModule, DxDataGridModule } from 'devextreme-angular';
+import { NavbarTopComponent } from './Components/navbar/navbar-top/navbar-top.component';
 
 @NgModule({
   declarations: [
@@ -14,15 +18,17 @@ import { WordScoringComponent } from './Components/games/word-scoring/word-scori
     LoginComponent,
     RegisterComponent,
     HomeComponent,
-    WordScoringComponent
+    WordScoringComponent,
+    NavbarTopComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, FormsModule,
     AppRoutingModule
+    ,DxDataGridModule,DxButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA]  // 👈 เพิ่มบรรทัดนี้เข้าไป
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]  // 👈 เพิ่มบรรทัดนี้เข้าไป
 
 })
 export class AppModule { }
