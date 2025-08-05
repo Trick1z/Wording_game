d1970c84bd64138657f582a4130194739a5d98ef
trick1z <tiker.zone.teen@gmail.com>
2025-08-05 15:02:24 +0700

    ดักข้อมูล register

--- src/app/Components/Pages/register/register.component.ts
+++ src/app/Components/Pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  customerRole = [
+    { value: false, name: 'Regular' },
+    { value: true, name: 'VIP' },
 
+
+  ];
+  registerData: RegisterData = {
+
+    username: null,
+    password: null,
+    confirmPassword: null,
+    isVip: null
+  }
+
+  onSubmit() {
+
+    if (!this.registerData.username?.trim() ||
+      !this.registerData.password?.trim() ||
+      !this.registerData.confirmPassword?.trim() ||
+      this.registerData.isVip === null) {
+
+      return Swal.fire({
+        title: "กรุณาใส่ข้อมูลให้ครบถ้วน",
+        icon: "error",
+        draggable: true
+      });
+
+
+    }
+
+    if (this.registerData.password !== this.registerData.confirmPassword) {
+      return Swal.fire({
+        title: "รหัสผ่านไม่ตรงกัน",
+        icon: "error",
+        draggable: true
+      });
+    }
+
+
+    // true
+    return console.log(this.registerData);
+  }
 }
+
+interface RegisterData {
+  username: string | null
+  password: string | null
+  confirmPassword: string | null
+  isVip: boolean | null
+
+} 
