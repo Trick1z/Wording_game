d3ebfcb69709be371a519dd3656649e0a3a06019
trick1z <tiker.zone.teen@gmail.com>
2025-08-05 12:04:04 +0700

    เพิ่ม Api service

--- src/app/Services/api-service.service.ts
+++ src/app/Services/api-service.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class ApiServiceService {
+export class ApiService {
+  private baseUrl = 'http://localhost:5000';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  get(endpoint: string): Observable<any> {
+    const url = `${this.baseUrl}/${endpoint}`;
+    return this.http.get(url);
+  }
+
+  post(endpoint: string, data: any): Observable<any> {
+    const url = `${this.baseUrl}/${endpoint}`;
+    return this.http.post(url, data);
+  }
+
+  put(endpoint: string, data: any): Observable<any> {
+    const url = `${this.baseUrl}/${endpoint}`;
+    return this.http.put(url, data);
+  }
+
+ delete(endpoint: string, data: any): Observable<any> {
+  const url = `${this.baseUrl}/${endpoint}`;
+  return this.http.delete(url, { body: data });
+}
 }
