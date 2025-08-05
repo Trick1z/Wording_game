512cccbcbc8d4ba790f1e05913aca2c59564fd45
trick1z <tiker.zone.teen@gmail.com>
2025-08-05 12:02:26 +0700

    ใส่ authguard ,redirect . noauth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Components/Pages/login/login.component';
-import { AuthRoute, GamesRoute, ViewsRoute} from './Constants/routes.const';
+import { AuthRoute, GamesRoute, ViewsRoute } from './Constants/routes.const';
 import { ViewsRoutingModule } from './Components/views/views-routing.module';
+import { AuthGuard } from './guards/auth.guard';
+import { NoAuthGuard } from './guards/no-auth.guard';
+import { RedirectGuard } from './guards/redirect.guard';
 
 const routes: Routes = [
 
@@ -13,17 +16,37 @@ const routes: Routes = [
       {
         path: AuthRoute.prefix,
         loadChildren: () => import('./Components/Pages/pages-routing.module').then(m => m.PagesRoutingModule)
+
+        , canActivate: [NoAuthGuard],
       },
       {
-        path : ViewsRoute.prefix,
-        loadChildren :() => import ('./Components/views/views-routing.module').then(m => m.ViewsRoutingModule)
+        canActivate: [AuthGuard],
+
+        path: ViewsRoute.prefix,
+        loadChildren: () => import('./Components/views/views-routing.module').then(m => m.ViewsRoutingModule)
       },
       {
-        path : GamesRoute.prefix,
-        loadChildren :() => import ('./Components/games/games-routing.module').then(m => m.GamesRoutingModule)
+        canActivate: [AuthGuard],
+        path: GamesRoute.prefix,
+        loadChildren: () => import('./Components/games/games-routing.module').then(m => m.GamesRoutingModule)
       }
     ]
-  }
+
+
+  },
+
+  {
+    path: '',
+    component: LoginComponent,
+    canActivate: [RedirectGuard],
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: LoginComponent,
+    canActivate: [RedirectGuard],
+  },
+
 
 
 
