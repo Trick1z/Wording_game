f3fbabf9dee21da5f8cf97546008ea82a6291d06
trick1z <tiker.zone.teen@gmail.com>
2025-08-09 13:21:05 +0700

    เพิ้ม constance CustomerForm , CustomerAddForm

--- src/app/Constants/routes.const.ts
+++ src/app/Constants/routes.const.ts
@@ -1,26 +1,36 @@
 export class AuthRoute {
-    static prefix: string = `auth`;
+  static prefix: string = `auth`;
 
-    static Login: string = `login`;
-    static LoginFullPath: string = `${AuthRoute.prefix}/${AuthRoute.Login}`;
-    // register path
-    static Register: string = `register`;
-    static RegisterFullPath: string = `${AuthRoute.prefix}/${AuthRoute.Register}`;
-   
-    // static LandingPage: string = `Landing`;
-    // static LandingPageFullPath: string = `${AuthRoute.prefix}/${AuthRoute.LandingPage}`;
-    
+  static Login: string = `login`;
+  static LoginFullPath: string = `${AuthRoute.prefix}/${AuthRoute.Login}`;
+  // register path
+  static Register: string = `register`;
+  static RegisterFullPath: string = `${AuthRoute.prefix}/${AuthRoute.Register}`;
+
+  // static LandingPage: string = `Landing`;
+  // static LandingPageFullPath: string = `${AuthRoute.prefix}/${AuthRoute.LandingPage}`;
 }
 
 export class ViewsRoute {
-    static prefix: string = `view`;
+  static prefix: string = `view`;
 
-    static Home: string = `home`;
-    static HomeFullPath: string = `${ViewsRoute.prefix}/${ViewsRoute.Home}`;
+  static Home: string = `home`;
+  static HomeFullPath: string = `${ViewsRoute.prefix}/${ViewsRoute.Home}`;
 }
 export class GamesRoute {
-    static prefix: string = `game`;
+  static prefix: string = `game`;
 
-    static Word: string = `word-scoring`;
-    static WordFullPath: string = `${GamesRoute.prefix}/${GamesRoute.Word}`;
+  static Word: string = `word-scoring`;
+  static WordFullPath: string = `${GamesRoute.prefix}/${GamesRoute.Word}`;
+}
+
+
+export class CustomerRoute {
+  static prefix: string = `customer`;
+
+  static CustomerForm: string = `form`;
+  static CustomerFormFullPath: string = `${GamesRoute.prefix}/${GamesRoute.Word}`;
+
+  static CustomerAddForm: string = `add-form`;
+  static CustomerAddFormFullPath: string = `${GamesRoute.prefix}/${GamesRoute.Word}`;
 }
