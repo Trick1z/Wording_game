a979e0ac0e9a8d77cc6015eca84804be94b74c20
trick1z <tiker.zone.teen@gmail.com>
2025-08-13 00:15:07 +0700

    เพิ่ม interface update

--- src/app/Components/models/categories.model.ts
+++ src/app/Components/models/categories.model.ts
@@ -9,5 +9,19 @@ export interface ProductDeleteFormData {
   productName: string ;
 
 } 
+
+export interface ProductUpdateFormData {
+  productId: number;
+  productName: string ;
+
+} 
+ 
+
+export interface CategoriesUpdateFormData {
+issueCategoriesId: number;
+  issueCategoriesName: string ;
+  isProgramIssue: boolean;
+} 
+ 
  
  
